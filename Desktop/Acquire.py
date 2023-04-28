7892a57c1da4d15103ecb7da7947f161439ff473
Planesman22 <shawnsql@gmail.com>
2023-04-28 05:20:19 -0400

    PytorchTest

--- Desktop/Acquire.py
+++ Desktop/Acquire.py
@@ -13,25 +13,54 @@ Classes = YoloModel.names
 # Get Camera Frames :)
 Camera = cv2.VideoCapture(0)
 
+_, LastFrame = Camera.read()
+
 while Camera.isOpened():
-    Success, Frame = Camera.read()
-    if Success:
-        # Display the frame
-        cv2.imshow('Webcam', Frame)
+    _, Frame = Camera.read()
+    FrameWidth = Frame.shape[1]
+    FrameHeight = Frame.shape[0]
 
-        FrameNumpy = [Frame]
-        Results = YoloModel(FrameNumpy)
-        Labels = Results.xyxyn[0][:, -1].cpu().numpy()
-        Cordinates = Results.xyxyn[0][:, :-1].cpu().numpy()
+    FrameNumpy = [Frame]
+    Results = YoloModel(FrameNumpy).xyxyn[0].cpu().numpy()
 
-        os.system("clear")
+    # Generate our feature  vector
+    PVector = numpy.zeros((len(Classes), 4))
+    for I in range(PVector.shape[0]):
+        PVector[I, 0] = I
 
-        for I in range(len(Labels)):
-            print(Classes[Labels[I]])
+    # Class probabilities and Size
+    for I in Results:
+        Class = int(I[5])
+        Probability = I[4]
+        if Probability > PVector[Class, 1]:
+            TargetX = I[0]
+            TargetY = I[1]
+            TargetW = I[2]
+            TargetH = I[3]
 
+            PVector[Class, 1] = Probability
+            PVector[Class, 2] = TargetW * TargetH
 
-    else:
-        print("Failed to get frame!")
+            # Crops
+            Y1 = int(TargetY * FrameHeight)
+            Y2 = int(TargetH * FrameHeight)
+            X1 = int(TargetX * FrameWidth)
+            X2 = int(TargetW * FrameWidth)
+            # cv2.imshow(Classes[Class], Frame[Y1:Y2,X1:X2])
+
+            # Optical Flow, gray scale only
+            GrayA = cv2.cvtColor(LastFrame[Y1:Y2,X1:X2], cv2.COLOR_BGR2GRAY)
+            GrayB = cv2.cvtColor(Frame[Y1:Y2,X1:X2], cv2.COLOR_BGR2GRAY)
+            Flow = cv2.calcOpticalFlowFarneback(GrayA, GrayB, None, pyr_scale=0.5, levels=3, winsize=15,
+                                                iterations=3, poly_n=5, poly_sigma=1.1, flags=0)
+            PVector[Class, 3] = numpy.mean(cv2.cartToPolar(Flow[..., 0], Flow[..., 1])[0])
+
+            # Update Last Frame
+    LastFrame = Frame.copy()
+
+    os.system("clear")
+    print(PVector)
+    # We cut the frame
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
