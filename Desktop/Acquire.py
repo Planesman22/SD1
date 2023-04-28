8675bd265dca9c55905e7cfeaa1b9d6dd02a01d5
Planesman22 <shawnsql@gmail.com>
2023-04-27 22:53:33 -0400

    Desktop Only Mode

--- Desktop/Acquire.py
+++ Desktop/Acquire.py
@@ -3,4 +3,19 @@ import torch
 import numpy
 import sys
 
+# Load Model
+YoloModel = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+YoloModel.to('GPU')
 
+# Get Camera Frames :)
+Camera = cv2.VideoCapture(0)
+
+while Camera.isOpened():
+    Success, Frame = Camera.read()
+
+    cv2.imshow('Webcam', Frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+player.release()
+cv2.destroyAllWindows()
