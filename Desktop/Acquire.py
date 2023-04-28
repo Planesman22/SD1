8ece2e3e9bdc7afd3908e81ea738e3dbbc313402
Planesman22 <shawnsql@gmail.com>
2023-04-28 01:20:21 -0400

    Cleanup

--- Desktop/Acquire.py
+++ Desktop/Acquire.py
@@ -2,18 +2,33 @@ import cv2
 import torch
 import numpy
 import sys
+import os
+
 
 # Load Model
 YoloModel = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 YoloModel.to('cuda')
+Classes = YoloModel.names
 
 # Get Camera Frames :)
 Camera = cv2.VideoCapture(0)
 
 while Camera.isOpened():
     Success, Frame = Camera.read()
+    if Success:
+        FrameNumpy = [Frame]
+        Results = YoloModel(FrameNumpy)
+        Labels = Results.xyxyn[0][:, -1].cpu().numpy()
+        Cordinates = Results.xyxyn[0][:, :-1].cpu().numpy()
 
-    cv2.imshow('Webcam', Frame)
+        for I in range(len(Labels)):
+            print(Classes[Labels[I]])
+
+
+        cv2.imshow('Webcam', Frame)
+    else:
+        print("Failed to get frame!")
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
